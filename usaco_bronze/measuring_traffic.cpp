cc1ae216f845d39b7abe1c03fc1a0ebbda6af66c
sp2044 <sp2044@srmist.edu.in>
2025-01-31 10:56:21 +0530

    learning on 31st Jan, 2025

---
+++ usaco_bronze/measuring_traffic.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FastIO cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+	freopen("traffic.in", "r", stdin);
+	freopen("traffic.out", "w", stdout);
+}
+
+signed main() {
+   FastIO
+   int TC=1;
+   // cin>>TC;
+   for(int i=0;i<TC;i++) {
+   	// cout<<"Case #"<<TC<<": ";
+      solve();
+   }
+   return 0;
+}
